9803723f3ce80ca46f5bd0afccd2fc3c84dea3fc
fudgeejane <francinejane.sd@gmail.com>
2026-05-26 10:14:50 +0800

    auth helpers link

--- src/utils/authHelpers.js
+++ src/utils/authHelpers.js
@@ -1,6 +1,10 @@
 const getPasswordResetContinueUrl = () => {
   if (typeof window !== 'undefined' && window.location && window.location.origin) {
-    return `${window.location.origin}/#/reset-password`;
+    const { origin, protocol } = window.location;
+
+    if (protocol === 'http:' || protocol === 'https:') {
+      return `${origin}/#/reset-password`;
+    }
   }
   return 'https://tcc-ccs-faculty.web.app/#/reset-password';
 };
